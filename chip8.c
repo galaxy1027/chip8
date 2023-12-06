f111824a55551f23d02447b8b849df0d45e8bc25
galaxy1027 <branford2001@gmail.com>
2023-12-05 21:33:14 -0800

    Added execute method

--- chip8.c
+++ chip8.c
@@ -44,5 +44,40 @@ void fetch(Chip8* chip8) {
 }
 
 void execute(Chip8* chip8) {
+    uint8_t n4 = chip8->opcode & 0x000F;
+    uint8_t n3 = (chip8->opcode >> 4) & 0x000F;
+    uint8_t n2 = (chip8->opcode >> 8) & 0x000F;
+    uint8_t n1 = (chip8->opcode >> 12) & 0x000F;
 
+    switch(n1) {
+        case 0x0:
+            switch (n4) {
+                case 0x0:
+                    clearScreen(chip8);
+                    break;
+                case 0xE:
+                    break;
+            }
+            break;
+
+        case 0x1:
+            jump(chip8);
+            break;
+
+        case 0x6:
+            set(chip8);
+            break;
+        
+        case 0x7:
+            add(chip8);
+            break;
+
+        case 0xA:
+            set(chip8);
+            break;
+
+        case 0xD:
+            draw(chip8);
+            break;
+    }
 }
